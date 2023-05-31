0114d319b8db2a3c22ef3a7c96ace4d18247024f
jihoon_sung <jihoon_sung@tmax.co.kr>
2023-05-31 15:31:16 +0900

    change localhost tests & add failed test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,19 +1,30 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+  await page.goto("http://localhost:3000/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/React App/);
 });
 
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+test("has text", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+
+  await expect(page.locator("#root")).toHaveText(/Vue/);
+});
+
+test("get started link", async ({ page, context }) => {
+  await page.goto("http://localhost:3000/");
+
+  // Start waiting for new page before clicking. Note no await.
+  const pagePromise = context.waitForEvent("page");
 
   // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
+  await page.getByRole("link", { name: "Learn React" }).click();
+  const linkPage = await pagePromise;
+  await linkPage.waitForLoadState();
+  console.log(await linkPage.title());
 
   // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-  await expect(page).toHaveURL(/.*atro/);
+  await expect(linkPage).toHaveURL(/react.dev/);
 });
